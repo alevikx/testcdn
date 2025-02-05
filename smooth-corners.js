69331e3b3b822cec79bbf968266c91e66c042586
alevikx <131615202+alevikx@users.noreply.github.com>
2025-02-05 15:41:10 +0530

    Update smooth-corners.js

--- smooth-corners.js
+++ smooth-corners.js
@@ -1,39 +1,41 @@
-class SmoothCorners {
+registerPaint('smooth-corners', class {
     static get inputProperties() {
-        return ['--smooth-corners'];
+        return [
+            '--smooth-corners'
+        ]
     }
+    paint(ctx, geom, properties) {
+        const c = properties.get('--smooth-corners').toString()
 
-    paint(ctx, size, properties) {
-        const n = properties.get('--smooth-corners').value || 4;
-        const width = size.width;
-        const height = size.height;
-        const m = Math.max(0.00000000001, Math.min(n, 100));
-        const r = width / 2;
-        const w = width / 2;
-        const h = height / 2;
+        ctx.fillStyle = 'black'
 
-        ctx.fillStyle = 'black';
+        const n = c
+        let m = n
+        if (n > 100) m = 100
+        if (n < 0.00000000001) m = 0.00000000001
+        const r = geom.width / 2
+        const w = geom.width / 2
+        const h = geom.height / 2
+
         ctx.beginPath();
 
-        for (let i = 0; i <= 2 * r; i++) {
-            const x = (i - r) + w;
-            const y = Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(i - r), m)), 1 / m) + h;
-            if (i === 0) {
-                ctx.moveTo(x, y);
-            } else {
-                ctx.lineTo(x, y);
-            }
+        for (let i = 0; i < (2*r+1); i++) {
+            const x = (i-r) + w
+            const y = (Math.pow(Math.abs(Math.pow(r,m)-Math.pow(Math.abs(i-r),m)),1/m)) + h
+
+            if (i == 0)
+                ctx.moveTo(x, y)
+            else
+                ctx.lineTo(x, y)
         }
 
-        for (let i = 2 * r; i <= 4 * r; i++) {
-            const x = (3 * r - i) + w;
-            const y = -Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(3 * r - i), m)), 1 / m) + h;
-            ctx.lineTo(x, y);
+        for (let i = (2*r); i < (4*r+1); i++) {
+            const x = (3*r-i) + w
+            const y = (-Math.pow(Math.abs(Math.pow(r,m)-Math.pow(Math.abs(3*r-i),m)),1/m)) + h
+            ctx.lineTo(x, y)
         }
 
-        ctx.closePath();
-        ctx.fill();
+        ctx.closePath()
+        ctx.fill()
     }
-}
-
-registerPaint('smooth-corners', SmoothCorners);
+})
